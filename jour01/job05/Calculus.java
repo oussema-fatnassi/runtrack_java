0682d75410399b605c8336a3c6191976fcec6559
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-06-10 15:47:12 +0200

    Calculus class to print out value of number after operations

---
+++ jour01/job05/Calculus.java
@@ -0,0 +1,12 @@
+package jour01.job05;
+
+public class Calculus {
+    public static void main(String[] args) {
+        int number = 7;
+        System.out.println(number);
+        number = number + 3;
+        System.out.println(number);
+        number = number * 5;
+        System.out.println(number);
+    }
+}
