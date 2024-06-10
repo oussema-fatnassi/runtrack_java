3328c755027ce0f7fed041d7e58d3da1d4873514
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-06-10 14:53:44 +0200

    Write comments in different ways (one line and multiple lines)

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,12 @@
+package jour01.job03;
+
+public class HelloWorld {                               // This class is the class declaration
+    public static void main(String[] args) {           
+        System.out.println("Hello World");
+    }
+}
+
+/*
+ This way we can write comments 
+ in Java in multiple lines
+ */
