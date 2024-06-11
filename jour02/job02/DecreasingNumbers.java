a40d75c72dbf39336c81699d62ebb60c5375ff4b
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-06-11 09:22:43 +0200

    Print numbers from 100 to 1

---
+++ jour02/job02/DecreasingNumbers.java
@@ -0,0 +1,9 @@
+package jour02.job02;
+
+public class DecreasingNumbers {
+    public static void main(String[] args) {
+        for (int i = 100; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
