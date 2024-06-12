4a2a5e5cb103d699a51a477575fe0465f4a37fc8
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-06-12 14:14:00 +0200

    Sum two matrices

---
+++ jour03/job07/MatrixSum.java
@@ -0,0 +1,45 @@
+package jour03.job07;
+
+public class MatrixSum {
+    public static void main(String[]args){
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("Matrix 1:");
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int[][] matrix2 = {
+            {10, 20, 30},
+            {40, 50, 60},
+            {70, 80, 90}
+        };
+        System.out.println("Matrix 2:");
+        for (int i = 0; i < matrix2.length; i++) {
+            for (int j = 0; j < matrix2[i].length; j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int[][] sum = new int[matrix1.length][matrix1[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("Matrix 1 + Matrix 2:");
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = 0; j < sum[i].length; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
